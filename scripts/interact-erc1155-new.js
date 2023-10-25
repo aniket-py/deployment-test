c9d2173baf48e81b46ff7aa9fc0172e5f6cf35ff
aniket-py <aniketmahida.py@gmail.com>
2023-10-26 03:47:38 +0530

    Update interact-erc1155-new.js

--- scripts/interact-erc1155-new.js
+++ scripts/interact-erc1155-new.js
@@ -15,23 +15,24 @@ async function main() {
   // Connect to the ERC-1155 contract
   const erc1155Contract = await ethers.getContractAt("MyExtendedERC1155Token", contractAddress);
 
-  // Call the mintFungible function
+  // Call the mint function
   const tokenIdToFungibleMint = 1; // Replace with the actual token ID
   const amountToFungibleMint = 100; // Replace with the actual amount
-  const mintFungibleTx = await erc1155Contract.mintFungible(tokenIdToFungibleMint, amountToFungibleMint);
-  await mintFungibleTx.wait();
+  const mintTx = await erc1155Contract.mint(process.env.Your_Address, tokenIdToFungibleMint, amountToFungibleMint, "0x");
+  await mintTx.wait();
   console.log(`Minted ${amountToFungibleMint} of ERC-1155 fungible token with ID ${tokenIdToFungibleMint}`);
 
-  // Call the mintNonFungible function
+  // Call the mintBatch function
   const tokenIdToNonFungibleMint = 2; // Replace with the actual token ID
-  const mintNonFungibleTx = await erc1155Contract.mintNonFungible(process.env.Your_Address, tokenIdToNonFungibleMint);
-  await mintNonFungibleTx.wait();
+  const amountsToMint = [1]; // Replace with the actual amount
+  const mintBatchTx = await erc1155Contract.mintBatch(process.env.Your_Address, [tokenIdToNonFungibleMint], amountsToMint, "0x");
+  await mintBatchTx.wait();
   console.log(`Minted an ERC-1155 non-fungible token with ID ${tokenIdToNonFungibleMint}`);
 
   // Call the burn function
   const tokenIdToBurn = 1; // Replace with the actual token ID
   const amountToBurn = 10; // Replace with the actual amount to burn
-  const burnTx = await erc1155Contract.burn(tokenIdToBurn, amountToBurn);
+  const burnTx = await erc1155Contract.burn(process.env.Your_Address, tokenIdToBurn, amountToBurn);
   await burnTx.wait();
   console.log(`Burned ${amountToBurn} of ERC-1155 token with ID ${tokenIdToBurn}`);
 }
